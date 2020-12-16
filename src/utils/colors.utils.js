660e1631c5e1b5c89fc3dcffcdbfbbb658662513
lolmos <lolmos@3oclock.co>
2020-12-16 23:49:57 +0100

    testing to see why it's hanking in heroku

--- src/utils/colors.utils.js
+++ src/utils/colors.utils.js
@@ -1,10 +1,11 @@
-// set the ulr 
-const colorsURL = "http://www.colr.org/json/colors/random/10"
 
 // function for doing async call and returning colors array
 // takes in the hook to use and then returns the hook withh the response as a paramater
 // 
 const getColors = async (setterHook) => {
+  // set the ulr 
+  const colorsURL = "http://www.colr.org/json/colors/random/10"
+
   const fetchColors = async () => await fetch(colorsURL)
     .then(response => response.json())
     .then(data => setterHook(data.colors));

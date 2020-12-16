f11322e3531b5a95d17ef0ada33a9ddb96ed6cc2
lolmos <lolmos@3oclock.co>
2020-12-16 23:15:27 +0100

    removed original examples set up the app as per instructions

--- src/App.js
+++ src/App.js
@@ -1,55 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import List from './components/List/List.component';
+import ListItemCreator from './components/ListItemCreator/ListItemCreator.component';
+import getColors from './utils/colors.utils'
+
+
 
 function App() {
+  // set initial state for colors
+  const [colors, setColors] = useState(null)
+
+  useEffect(() => {
+    // on component mount run sync function to get colors
+    getColors(setColors)
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        <h1>Simple TODO List App</h1>
+
+        { /** Display a loading message while the colors load */
+          colors === null && <span>Loading..</span>
+        }
+
+        { //Display the parts if the colors have loaded
+          colors !== null &&
+          <div class="wrapper">
+            <ListItemCreator colors={colors} firstColor={colors[0].hex} />
+            <List />
+          </div>
+        }
       </header>
     </div>
   );

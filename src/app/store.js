11659e9d511d11be8324afa652f347a0fd2b62f8
lolmos <lolmos@3oclock.co>
2020-12-16 23:23:03 +0100

    importing list reducer and updating store object

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import listReducer from '../components/ListItemCreator/listSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
+    lists: listReducer
   },
 });
